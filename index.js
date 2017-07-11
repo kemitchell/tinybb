582cb8b37e4d077b4a2a890b83a4dc4ed32ccd15
Kyle E. Mitchell <kyle@kemitchell.com>
2017-07-10 21:18:14 -0700

    WIP: Passport

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
+var passport = require('passport')
+var cookieParser = require('cookie-parser')
+var expressSession = require('express-session')
+var connectFlash = require('connect-flash')
+
 var app = module.exports = require('express')()
 
 app.use(require('./logger'))
+// For flash:
+app.use(cookieParser('secret'))
+app.use(expressSession({cookie: {maxAge: 60000}}))
+app.use(connectFlash())
 
 app.get('/', function (request, response) {
   response.end()
 })
+
+app.get('/login', passport.authenticate('local', {
+  successRedirect: '/',
+  failureRedirect: '/login',
+  failureFlash: true
+}))
